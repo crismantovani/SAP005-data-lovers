39f79593c6f9b18e1a729696c5bae6c1c4e27f1a
Thais Wemberlaine <thaiswemberlaine@gmail.com>
2020-12-02 21:26:52 -0300

    Filter rarity

--- src/main.js
+++ src/main.js
@@ -28,5 +28,17 @@ const cardSectionFilter = document.getElementById("container-filter")
 cardSectionFilter.innerHTML = cards(filterResult) 
 }
 
-let buttonFilter = document.getElementById("filter")
+let buttonFilter = document.getElementById("filter-type")
 buttonFilter.addEventListener ("click", filter)
+
+function filterRarity () {
+  let chosenRarity = document.getElementById("chosen-rarity").value
+  let filterRarityResult = pokemonsArray.filter(pokemon => {
+  return pokemon.rarity.includes(chosenRarity)
+})
+const cardSectionRarityFilter = document.getElementById("container-filter")
+cardSectionRarityFilter.innerHTML = cards(filterRarityResult) 
+}
+
+let buttonFilterRarity = document.getElementById("filter-rarity")
+buttonFilterRarity.addEventListener ("click", filterRarity)
