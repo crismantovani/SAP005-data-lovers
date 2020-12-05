7f3f60d0c96da2015b8699d9360c49ca9d61d04d
crismantovani <crismantovani87@gmail.com>
2020-12-05 04:11:02 -0300

    Comments added

--- src/data.js
+++ src/data.js
@@ -1,11 +1,12 @@
 //Pega string do HTML e compara com o objeto type do data e retorna o que for igual
+//Get HTML string and compare with data's type object and return what is the same
 export const showFilterType = (data, userText) => {
-  const typeInclude = (data.filter (data => data.type.includes(userText)))
+  const typeInclude = (data.filter(data => data.type.includes(userText)))
   return (typeInclude);
 };
 
+//Get HTML string and compare with data's rarity object and return what is the same
 export const showFilterRarity = (data, userText) => {
-  const rarityInclude = (data.filter (data => data.rarity.includes (userText)))
+  const rarityInclude = (data.filter(data => data.rarity.includes(userText)))
   return (rarityInclude);
 };
-//
--- src/main.js
+++ src/main.js
@@ -1,28 +1,38 @@
+//Importation modules 'import | export'
 import data from './data/pokemon/pokemon.js';
-import {showFilterType, showFilterRarity} from './data.js';
-//
+import {
+  showFilterType,
+  showFilterRarity
+} from './data.js';
+
+//The array goes through the pokemon.js and displays the card 
+//with the configured attributes on the home page
 const pokemonsArray = data.pokemon
-function cards (array) {
-    let card = ""
-    for (let pokemon of array) {
-        card += `
+
+function cards(array) {
+  let card = ""
+  for (let pokemon of array) {
+    card += `
         <article class="card">
           <h3 class="card-title">${pokemon.name}</h3>
           <img class="card-image" src=${pokemon.img}>
           <p class="card-type">${pokemon.type}</p>
           <p class="card-rarity">${pokemon.rarity}</p>
         </article>`
-    }
-    return card
+  }
+  return card
 }
 
 const cardSection = document.getElementById("container-main")
 cardSection.innerHTML = cards(pokemonsArray)
 
-function cardsInfo (array) {
+//The array goes through the pokemon.js and displays the card 
+//with the configured attributes on the home page
+
+function cardsInfo(array) {
   let cardInfo = ""
   for (let pokemon of array) {
-      cardInfo += `
+    cardInfo += `
       <article class="card-info">
         <p class="card-type">${pokemon.num}</p>
         <h3 class="card-title">${pokemon.name}</h3>
@@ -39,42 +49,46 @@ function cardsInfo (array) {
   return cardInfo
 }
 
-function filterType (event) {
+//Function to filter by type
+
+function filterType(event) {
   event.preventDefault()
   let chosenType = document.getElementById("chosen-type").value
   let filterResult = pokemonsArray.filter(pokemon => {
-  return pokemon.type.includes(chosenType)
-})
-let percentageCal = parseInt(((filterResult.length)*100)/(pokemonsArray.length))
-const percentageFilter = document.getElementById("type-percentage")
-percentageFilter.textContent = "Os pokémons do tipo " + chosenType + " representam " + percentageCal + "% dos pokémons totais da região de Kanto"
+    return pokemon.type.includes(chosenType)
+  })
+  let percentageCal = parseInt(((filterResult.length) * 100) / (pokemonsArray.length))
+  const percentageFilter = document.getElementById("type-percentage")
+  percentageFilter.textContent = "Os pokémons do tipo " + chosenType + " representam " + percentageCal + "% dos pokémons totais da região de Kanto"
 
-const cardSectionFilter = document.getElementById("container-filter")
-cardSectionFilter.innerHTML = cardsInfo(filterResult) 
+  const cardSectionFilter = document.getElementById("container-filter")
+  cardSectionFilter.innerHTML = cardsInfo(filterResult)
 }
 
 let buttonFilter = document.getElementById("filter-type")
-buttonFilter.addEventListener ("click", filterType)
+buttonFilter.addEventListener("click", filterType)
 
+//Function to filter by Rarity
 
-function filterRarity (event) {
+function filterRarity(event) {
   event.preventDefault()
   let chosenRarity = document.getElementById("chosen-rarity").value
   let filterRarityResult = pokemonsArray.filter(pokemon => {
-  return pokemon.rarity.includes(chosenRarity)
-})
-let percentageCalRarity = parseInt(((filterRarityResult.length)*100)/(pokemonsArray.length))
-const percentageFilterRarity = document.getElementById("type-percentage")
-percentageFilterRarity.textContent = "Os pokémons de raridade " + chosenRarity + " representam " + percentageCalRarity + "% dos pokémons totais da região de Kanto"
+    return pokemon.rarity.includes(chosenRarity)
+  })
+  let percentageCalRarity = parseInt(((filterRarityResult.length) * 100) / (pokemonsArray.length))
+  const percentageFilterRarity = document.getElementById("type-percentage")
+  percentageFilterRarity.textContent = "Os pokémons de raridade " + chosenRarity + " representam " + percentageCalRarity + "% dos pokémons totais da região de Kanto"
 
-const cardSectionRarityFilter = document.getElementById("container-filter")
-cardSectionRarityFilter.innerHTML = cardsInfo(filterRarityResult) 
+  const cardSectionRarityFilter = document.getElementById("container-filter")
+  cardSectionRarityFilter.innerHTML = cardsInfo(filterRarityResult)
 }
 
 let buttonFilterRarity = document.getElementById("filter-rarity")
 buttonFilterRarity.addEventListener("click", filterRarity)
 
-function cleanFilter () {
+//Function to clean the previously used filter
+function cleanFilter() {
   const cardSectionRarityFilter = document.getElementById("container-filter")
   cardSectionRarityFilter.innerHTML = ""
   const cardSectionRarityCal = document.getElementById("type-percentage")
@@ -84,23 +98,24 @@ function cleanFilter () {
 let buttonCleanFilter = document.getElementById("clean-button")
 buttonCleanFilter.addEventListener("click", cleanFilter)
 
-function sortPokemon (event) {
+//Function for sorting from AtoZ and ZtoA
+function sortPokemon(event) {
   event.preventDefault()
   let chosenOrder = document.getElementById("chosen-order").value
-  let order = (a,b) => (a["name"]).localeCompare(b["name"]);
+  let order = (a, b) => (a["name"]).localeCompare(b["name"]);
   let assortmentAZ = "";
   let assortmentZA = "";
 
   if (chosenOrder === "AaZ") {
-    let assortmentAZ = pokemonsArray.sort((a,b) => {
-      return order(a,b)
-  })
-  const cardOrder = document.getElementById("container-main")
-  cardOrder.innerHTML = cards(assortmentAZ)
-}
+    let assortmentAZ = pokemonsArray.sort((a, b) => {
+      return order(a, b)
+    })
+    const cardOrder = document.getElementById("container-main")
+    cardOrder.innerHTML = cards(assortmentAZ)
+  }
   if (chosenOrder === "ZaA") {
     let assortmentZA = pokemonsArray.sort((a, b) => {
-      return order(a,b)
+      return order(a, b)
     })
     assortmentZA.reverse()
     const cardOrder = document.getElementById("container-main")
@@ -109,16 +124,17 @@ function sortPokemon (event) {
 }
 
 let buttonOrder = document.getElementById("pokemon-order")
-buttonOrder.addEventListener ("click", sortPokemon)
+buttonOrder.addEventListener("click", sortPokemon)
 
-function cleanAssortment () {
-  let initialOrder = (a,b) => (a["num"]).localeCompare(b["num"]);
-    let assortmentInitial = pokemonsArray.sort((a,b) => {
-      return initialOrder(a,b)
+//Function to clear the sort
+function cleanAssortment() {
+  let initialOrder = (a, b) => (a["num"]).localeCompare(b["num"]);
+  let assortmentInitial = pokemonsArray.sort((a, b) => {
+    return initialOrder(a, b)
   })
   const cardOrder = document.getElementById("container-main")
   cardOrder.innerHTML = cards(assortmentInitial)
 }
 
 let buttonCleanOrder = document.getElementById("clean-order-filter")
-buttonCleanOrder.addEventListener ("click", cleanAssortment)
+buttonCleanOrder.addEventListener("click", cleanAssortment)
